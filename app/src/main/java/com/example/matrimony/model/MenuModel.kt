d4733d4bbcc04e1efbce66eef1ea5061f3721923
abhishekm <abhishekm@techepoch.com>
2021-07-23 10:52:47 +0530

    New Commit Matrimony

---
+++ app/src/main/java/com/example/matrimony/model/MenuModel.kt
@@ -0,0 +1,15 @@
+package com.example.matrimony.model
+
+class MenuModel {
+    var title: String? = null
+    var description: String? = null
+    var icon: String? = null
+//    var ctalink: String? = null
+//    var hasChildren = false
+//
+//    var iconName: Int? = null
+//
+//    constructor()
+
+
+}
